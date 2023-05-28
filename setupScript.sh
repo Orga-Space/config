cde5f9f96a79031716e9630a808ea9667faaf01f
Jan van den Berg <orga.space@posteo.org>
2023-05-28 12:02:47 +0200

    add update / install & moved config

--- setupScript.sh
+++ setupScript.sh
@@ -1,16 +1,23 @@
 SETUP_NAME=
 SETUP_EMAIL=
+DEFAULT_SHELL_CONFIG="${HOME}/.bashrc"
 
 ### install software ###
 sudo apt update
-sudo apt install htop tmux git wget curl -y
+sudo apt upgrade -y
+sudo apt install htop neofetch tmux git wget curl -y
 
 ### configure local git ###
 git config --global user.name "$SETUP_NAME"
 git config --global user.email "$SETUP_EMAIL"
 
 ### prepare connection to github ###
-GITHUB_PRIVATE_KEY="${HOME}/.ssh/github-id_ed25519"
+SSH_DIR="${HOME}/.ssh"
+GITHUB_PRIVATE_KEY="${SSH_DIR}/github-id_ed25519"
+if [ ! -d "$SSH_DIR" ]; then
+        mkdir "$SSH_DIR"
+fi
+
 if [ ! -f "$GITHUB_PRIVATE_KEY" ]; then
         ssh-keygen -t ed25519 -C "$SETUP_EMAIL" -f "$GITHUB_PRIVATE_KEY" -N ""
 echo "
@@ -20,18 +27,42 @@ Host github.com
 
 fi
 
+# todo all the file creation will be done with sudo, all the file will be owned by root, need to change the owner
+# all the command need to be executed from the user
+# https://askubuntu.com/questions/1033868/how-do-i-find-which-user-executes-the-script-when-is-used-sudo
+
 ### folder structure ###
 if [ ! -d "${HOME}/sandbox" ]; then
         mkdir "${HOME}/sandbox"
 fi
 
+if [ ! -d "${HOME}/sandbox/config" ]; then
+        git clone https://github.com/Orga-Space/config.git "${HOME}/sandbox/config"
+fi
+
+if [ ! -f "${HOME}/.aliases" ]; then
+        cp "${HOME}/sandbox/config/.aliases" "$HOME/.aliases"
+echo "
+if [ -f ~/.aliases ]; then
+    . ~/.aliases
+fi
+" >> "$DEFAULT_SHELL_CONFIG"
+fi
+
+if [ ! -f "${HOME}/.tmux.conf" ]; then
+        cp "${HOME}/sandbox/config/.tmux.conf" "$HOME/.tmux.conf"
+fi
+
+if [ ! -f "${HOME}/.vimrc" ]; then
+        cp "${HOME}/sandbox/config/.vimrc" "$HOME/.vimrc"
+fi
 
 #####  post run  #####
 
 ### ssh key added to ssh agent ###
 echo ""
 echo "### copy pub key for github ###"
-ecno "https://github.com/settings/keys"
+echo "https://github.com/settings/keys"
 echo ""
 cat "${GITHUB_PRIVATE_KEY}.pub"
 echo ""
